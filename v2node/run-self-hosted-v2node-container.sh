608207b9a88c9d4a8bb9c15d19f2e112bd298e11
Liangcheng Juves <liangchengj@outlook.com>
2022-06-20 03:16:09 +0000

    Fix: `Requested domain  is not a FQDN because it contains an empty label.`

--- v2node/run-self-hosted-v2node-container.sh
+++ v2node/run-self-hosted-v2node-container.sh
@@ -11,8 +11,6 @@ fi
 V2NODE_SELF_HOSTED_CONTAINER_ID=$(docker run -itd --privileged \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -p 443:443 \
-    -e mail=$mail \
-    -e domain=$domain \
     liangchengj/v2node:self-hosted /sbin/init)
 
-docker exec --workdir /root "$V2NODE_SELF_HOSTED_CONTAINER_ID" bash -e init-self-hosted-v2node.sh
+docker exec --workdir /root "$V2NODE_SELF_HOSTED_CONTAINER_ID" bash -e init-self-hosted-v2node.sh $mail $domain
