ceec6943961d506cd7da1495e7ab5ad0364af6fd
Liangcheng Juves <liangchengj@outlook.com>
2022-06-21 11:14:06 +0800

    Update v2node's images

--- v2node/run-self-hosted-v2node-container.sh
+++ v2node/run-self-hosted-v2node-container.sh
@@ -9,6 +9,7 @@ if [ -z "$mail" ]; then
 fi
 
 V2NODE_SELF_HOSTED_CONTAINER_ID=$(docker run -itd --privileged \
+    -h "$(echo "$domain" | base64)" \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -p 443:443 \
     liangchengj/v2node:self-hosted /sbin/init)
