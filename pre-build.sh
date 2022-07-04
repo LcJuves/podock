956d1be5818d9346359563fab19c696507d0260b
Liangcheng Juves <liangchengj@outlook.com>
2022-07-04 03:23:04 +0000

    Update how `LLVM 13` sources are added

--- pre-build.sh
+++ pre-build.sh
@@ -6,9 +6,6 @@ fi
 
 (
     cd macos-cross-compiler-builder || exit
-    if [ ! -f "llvm-toolchain-stretch-13.sources.list" ]; then
-        cp ../amuse-lang-environment/llvm-toolchain-stretch-13.sources.list .
-    fi
     if [ ! -f "cmake-3.23.1-linux-x86_64.tar.gz" ]; then
         curl -L -O https://github.com/Kitware/CMake/releases/download/v3.23.1/cmake-3.23.1-linux-x86_64.tar.gz
     fi
@@ -42,13 +39,6 @@ fi
     cd v2node || exit
     if [ ! -f "Python39.tar.xz" ]; then
         curl -L -O https://gitlab.com/LiangchengJ/podock/-/raw/main/py3-builder/Python39.tar.xz
-    fi
-)
-
-(
-    cd git-scm-builder || exit
-    if [ ! -f "llvm-toolchain-stretch-13.sources.list" ]; then
-        cp ../amuse-lang-environment/llvm-toolchain-stretch-13.sources.list .
     fi
 )
 
