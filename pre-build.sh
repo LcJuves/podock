5666aa26edcbd23d40163aec33bf97a3884fc7ba
Liangcheng Juves <liangchengj@outlook.com>
2022-12-26 13:39:51 +0000

    Change username

--- pre-build.sh
+++ pre-build.sh
@@ -14,14 +14,14 @@ fi
 (
     cd codeoss-server || exit
     if [ ! -f "codeoss-server-linux-x64-web.tar.gz" ]; then
-        curl -L -O https://github.com/LiangchengJ/vscode/releases/download/dev_test_part17/codeoss-server-linux-x64-web.tar.gz
+        curl -L -O https://github.com/LcJuves/vscode/releases/download/dev_test_part17/codeoss-server-linux-x64-web.tar.gz
     fi
 )
 
 (
     cd v2node || exit
     if [ ! -f "Python39.tar.xz" ]; then
-        curl -L -O https://gitlab.com/LiangchengJ/podock/-/raw/main/py3-builder/Python39.tar.xz
+        curl -L -O https://gitlab.com/LcJuves/podock/-/raw/main/py3-builder/Python39.tar.xz
     fi
 )
 
@@ -38,6 +38,6 @@ fi
 (
     cd rustaceans || exit
     if [ ! -f "macos-cross-compiler.tar.xz" ]; then
-        curl -L -O -s https://github.com/LiangchengJ/macos-cross-compiler/releases/download/mac-os-cross-compiler/macos-cross-compiler.tar.xz
+        curl -L -O -s https://github.com/LcJuves/macos-cross-compiler/releases/download/mac-os-cross-compiler/macos-cross-compiler.tar.xz
     fi
 )
