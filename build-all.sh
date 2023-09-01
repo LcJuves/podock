d40cac86b3d275bfb31211d0f3905ebff9348746
Liangcheng Juves <email@lcjuves.com>
2023-09-02 01:52:31 +0800

    Add remove svg width and height implementation on `svg-worker` image

--- build-all.sh
+++ build-all.sh
@@ -2,17 +2,19 @@
 # Created at 2023/8/23 20:43
 # @author Liangcheng Juves
 
+_PUSH=false
 if [ -n "$CI_REGISTRY_USER" ] && [ -n "$CI_REGISTRY_PASSWORD" ] && [ -n "$CI_REGISTRY" ]; then
     docker login -u "$CI_REGISTRY_USER" -p "$CI_REGISTRY_PASSWORD" "$CI_REGISTRY"
+    _PUSH=true
 fi
 
 dart pub get
 
-dart run build.dart --push true --containerInfoFile containerImages1.json
+dart run build.dart --push $_PUSH --containerInfoFile containerImages1.json
 (docker images | awk 'NR!=1{print $3}' | xargs docker rmi -f) || echo >/dev/null
 
-dart run build.dart --push true --containerInfoFile containerImages2.json
+dart run build.dart --push $_PUSH --containerInfoFile containerImages2.json
 (docker images | awk 'NR!=1{print $3}' | xargs docker rmi -f) || echo >/dev/null
 
-dart run build.dart --push true --containerInfoFile containerImages3.json
+dart run build.dart --push $_PUSH --containerInfoFile containerImages3.json
 # (docker images | awk 'NR!=1{print $3}' | xargs docker rmi -f) || echo >/dev/null
