bd7f00075eda0a52c5e8937452e835bd23e76994
Liangcheng Juves <email@lcjuves.com>
2025-05-21 00:04:38 +0800

    [ Fix ] Error: OCI runtime error: /usr/bin/crun: controller `pids` is not available under /sys/fs/cgroup/machine.slice/libpod-4a4df1fa71390aaa159f32b467ff26f00fe56ef37ce9b0b38edef796df8f19a1.scope/container/cgroup.controllers

--- v2node/run-self-hosted-v2node-container.sh
+++ v2node/run-self-hosted-v2node-container.sh
@@ -12,6 +12,8 @@ if [ ! -f "/usr/bin/crun" ]; then
     curl -L -o /usr/bin/crun -s https://github.com/containers/crun/releases/download/1.21/crun-1.21-linux-amd64
 fi
 
+mount -t cgroup
+
 V2NODE_SELF_HOSTED_CONTAINER_ID=$(podman run --runtime /usr/bin/crun -itd --privileged \
     -h "$(echo "$domain" | base64)" \
     -v /run/podman/podman.sock:/run/podman/podman.sock \
