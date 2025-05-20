74ba31d0b84becdb3d589efa2faa49e1c4cb2678
Liangcheng Juves <email@lcjuves.com>
2025-05-20 05:43:23 +0000

    The v2node image run script runs by default in the crun OCI container runtime

--- v2node/run-self-hosted-v2node-container.sh
+++ v2node/run-self-hosted-v2node-container.sh
@@ -8,7 +8,11 @@ if [ -z "$mail" ]; then
     mail=mail@localhost
 fi
 
-V2NODE_SELF_HOSTED_CONTAINER_ID=$(docker run -itd --privileged \
+if [ ! -f "/usr/bin/crun" ]; then
+    curl -L -o /usr/bin/crun -s https://github.com/containers/crun/releases/download/1.21/crun-1.21-linux-amd64
+fi
+
+V2NODE_SELF_HOSTED_CONTAINER_ID=$(docker run --runtime /usr/bin/crun -itd --privileged \
     -h "$(echo "$domain" | base64)" \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -p 443:443 \
