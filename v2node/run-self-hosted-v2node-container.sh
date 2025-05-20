0f07979cb52e9aa28a4aa15fe7ee581e051ba069
Liangcheng Juves <email@lcjuves.com>
2025-05-21 01:04:22 +0800

    Revert "Update `v2node` image"
    This reverts commit c4095bcfe7c4896cb1788232a6da9ac3f7671bf6.

--- v2node/run-self-hosted-v2node-container.sh
+++ v2node/run-self-hosted-v2node-container.sh
@@ -12,12 +12,12 @@ if [ ! -f "/usr/bin/crun" ]; then
     curl -L -o /usr/bin/crun -s https://github.com/containers/crun/releases/download/1.21/crun-1.21-linux-amd64
 fi
 
-V2NODE_SELF_HOSTED_CONTAINER_ID=$(podman run --runtime /usr/bin/crun -itd --privileged \
+V2NODE_SELF_HOSTED_CONTAINER_ID=$(docker run --runtime /usr/bin/crun -itd --privileged \
     -h "$(echo "$domain" | base64)" \
-    -v /run/podman/podman.sock:/run/podman/podman.sock \
+    -v /var/run/docker.sock:/var/run/docker.sock \
     -p 443:443 \
     ghcr.io/lcjuves/v2node:self-hosted /sbin/init)
 
-podman exec -it \
+docker exec -it \
     --workdir /root "$V2NODE_SELF_HOSTED_CONTAINER_ID" \
     bash -e init-self-hosted-v2node.sh "$mail" "$domain"
--- v2node/self-hosted.Dockerfile
+++ v2node/self-hosted.Dockerfile
@@ -1,12 +1,13 @@
 FROM container-base
 RUN apt-get install -y curl
+RUN curl -fsSL https://gitlab.com/LcJuves/podock/-/raw/main/container-image-builder/install-docker-for-debian.sh | sh
 
 RUN apt-get install -y nginx net-tools
 EXPOSE 443
 
-RUN apt-get install -y certbot vim podman
+RUN apt-get install -y certbot vim
 WORKDIR /root
 ADD certbot_renew.sh .
 ADD localhost.conf /etc/nginx/conf.d/
-ADD curn /usr/bin/
+
 ADD init-self-hosted-v2node.sh .
