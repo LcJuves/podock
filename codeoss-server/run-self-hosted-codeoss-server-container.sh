4855dee44df17e4ea90c193ecfbfd9442aeedb85
Liangcheng Juves <email@lcjuves.com>
2025-06-10 09:20:41 +0800

    Update `codeoss-server` image

---
+++ codeoss-server/run-self-hosted-codeoss-server-container.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ -z "$domain" ]; then
+    domain=localhost
+fi
+
+if [ -z "$mail" ]; then
+    mail=mail@localhost
+fi
+
+if [ -z "$sec_key" ]; then
+    sec_key="$(date +%s)"
+fi
+
+mount -t cgroup
+
+CODEOSS_SERVER_SELF_HOSTED_CONTAINER_ID=$(podman run -itd --privileged \
+    -h "$(echo "$domain" | base64)" \
+    -v /var/run/podman/podman.sock:/var/run/podman/podman.sock \
+    -p 443:443 \
+    ghcr.io/lcjuves/codeoss-server:self-hosted /sbin/init)
+
+podman exec -it \
+    --workdir /root "$CODEOSS_SERVER_SELF_HOSTED_CONTAINER_ID" \
+    bash -e init-self-hosted-codeoss-server.sh "$mail" "$domain"
---
+++ codeoss-server/self-hosted.Dockerfile
@@ -0,0 +1,12 @@
+FROM container-base
+RUN apt-get install -y curl podman
+
+RUN apt-get install -y nginx net-tools
+EXPOSE 443
+
+RUN apt-get install -y certbot vim
+WORKDIR /root
+ADD certbot_renew.sh .
+ADD localhost.conf /etc/nginx/conf.d/
+
+ADD init-self-hosted-codeoss-server.sh .
